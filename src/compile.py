02263eff0256d8881ee71736e1964626eac9a5af
MaxLastBreath <yusei.tqnev@gmail.com>
2024-08-20 00:07:05 +0300

    Multi platform compiler and dependencies

--- src/compile.py
+++ src/compile.py
@@ -5,10 +5,21 @@ import os
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(script_dir)
-    command = [
-        "pyinstaller",
-        "console.py",
-        "--onefile",
-        f"--name=TOTK Console",
-    ]
+
+    if platform.system() == "Windows":
+    
+        command = [
+            "pyinstaller",
+            "console.py",
+            "--onefile",
+            f"--name=TOTK Console",
+        ]
+    elif platform.system() == "Linux":
+        command = [
+            "pyinstaller",
+            "console.py",
+            "--onefile",
+            f"--name=TOTK Console.AppImage",
+        ]
+
     subprocess.run(command, shell=True)
